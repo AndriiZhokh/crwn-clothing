7fde721dc42b60f87f39516f4a439515ba206791
Andrii Zhokh <andriizhokh@gmail.com>
2023-07-24 20:47:37 +0300

    created separate component for input element with styles

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import FormInput from '../form-input/form-input.component';
 
 const defaultFormFields = {
   displayName: '',
@@ -48,8 +49,8 @@ const SignUpForm = () => {
     <div>
       <h1>Sign Up with your email and password</h1>
       <form onSubmit={ handleSubmit }>
-        <label>Display Name</label>
-        <input
+        <FormInput
+          label="Display Name"
           type="text"
           required
           onChange={ handleChange }
@@ -57,8 +58,8 @@ const SignUpForm = () => {
           value={ displayName }
         />
 
-        <label>Email</label>
-        <input
+        <FormInput
+          label="Email"
           type="email"
           required
           onChange={ handleChange }
@@ -66,8 +67,8 @@ const SignUpForm = () => {
           value={ email }
         />
 
-        <label>Password</label>
-        <input
+        <FormInput
+          label="Password"
           type="password"
           required
           onChange={ handleChange }
@@ -75,8 +76,8 @@ const SignUpForm = () => {
           value={ password }
         />
 
-        <label>Consfirm Password</label>
-        <input
+        <FormInput
+          label="Consfirm Password"
           type="password"
           required
           onChange={ handleChange }
