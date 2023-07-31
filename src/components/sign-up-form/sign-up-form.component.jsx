61d6ba62d5d0020ea5e3bfaafd242f2c0b8aeca7
Andrii Zhokh <andriizhokh@gmail.com>
2023-07-31 20:38:35 +0300

    button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
+import './sign-up-form.styles.scss';
 import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
 
 const defaultFormFields = {
   displayName: '',
@@ -46,8 +48,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign Up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign Up with your email and password</span>
       <form onSubmit={ handleSubmit }>
         <FormInput
           label="Display Name"
@@ -85,7 +88,7 @@ const SignUpForm = () => {
           value={ confirmPassword }
         />
 
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
