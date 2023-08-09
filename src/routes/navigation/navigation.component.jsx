7e571732e920c2cc9d79a57652fc5573a701f38d
Andrii Zhokh <andriizhokh@gmail.com>
2023-08-09 20:30:23 +0300

    added cart template

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,8 @@ import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import './navigation.styles.scss';
 
@@ -23,7 +25,11 @@ const Navigation = () => {
           ) : (
             <Link className='nav-link' to='/auth'>SIGN IN</Link>
           )}
+          <CartIcon />
         </div>
+        {/* TODO: Create context for dropdown that will contain state whether dropdown is opened or not
+            TODO: Also this context should contain info about items that were added to the cart */}
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
