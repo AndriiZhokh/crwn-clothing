ca55e9068c28cc56e5e5ffee09418ac246e715f0
Andrii Zhokh <andrii.zhokh@amway.com>
2024-01-19 18:12:00 +0200

    fetching product categories form firestore and added simple navigation inside shop page for categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,19 +1,68 @@
 import './shop.styles.scss';
-import { useContext } from 'react';
-import { ProductsContext } from '../../contexts/products.context'
+import { Fragment, useContext, useEffect, useState } from 'react';
+import { CategoriesContext } from '../../contexts/categories.context'
 import ProductCard from '../../components/product-card/product-card.component';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const Products = ({ category, categoriesMap, onCategoryTitleClick }) => {
+  return (
+    <Fragment>
+      <h2 onClick={() => onCategoryTitleClick && onCategoryTitleClick(category)}>{ category }</h2>
+
+      <div className="products-container">
+        {
+          categoriesMap[category].map(product => (
+            <ProductCard key={ product.id } product={ product }></ProductCard>
+          ))
+        }
+      </div>
+    </Fragment>
+  );
+};
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
+  const [ selectedCategory, setSelectedCategory ] = useState(null);
+  const [ navigateTo, setNavigateTo ] = useState(null);
+  const { categoriesMap } = useContext(CategoriesContext);
+  const navigate = useNavigate();
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    setSelectedCategory(categoryId);
+  }, [ categoryId ]);
+
+  useEffect(() => {
+    if (navigateTo) {
+      navigate(`/shop/${navigateTo}`);
+    }
+
+    return () => {
+      setNavigateTo(null);
+    };
+  }, [ navigateTo, navigate ]);
 
   return (
-    <div className="products-container">
+    <Fragment>
       {
-        products.map(product => (
-          <ProductCard key={ product.id } product={ product }></ProductCard>
-        ))
+        selectedCategory
+        ? (
+          <Products
+            category={ selectedCategory }
+            categoriesMap={ categoriesMap }
+          />
+        )
+        : (
+          Object.keys(categoriesMap).map(title => 
+            <Products
+              key={title}
+              category={ title }
+              categoriesMap={ categoriesMap }
+              onCategoryTitleClick={(category) => setNavigateTo(category)}
+            />
+          )
+        ) 
       }
-    </div>
+    </Fragment>
   );
 };
 
