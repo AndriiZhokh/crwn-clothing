6cd5fc2647c80aa576373b4ea5ff49981450dc15
Andrii Zhokh <andriizhokh@gmail.com>
2024-01-23 11:24:02 +0200

    fixed error with undefined products in selected category

--- src/routes/shop/components/category-preview/category-preview.component.jsx
+++ src/routes/shop/components/category-preview/category-preview.component.jsx
@@ -1,18 +1,23 @@
 import './category-preview.styles.scss';
 
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import ProductCard from '../../../../components/product-card/product-card.component';
 
 const CategoryPreview = ({ category, categoriesMap, onCategoryTitleClick }) => {
+  const [ products, setProducts ] = useState([]);
 
-  const productsToDisplay = () => onCategoryTitleClick ? categoriesMap[category].slice(0, 4) : categoriesMap[category];
+  useEffect(() => {
+    const listOfProducts = onCategoryTitleClick ? categoriesMap[category].slice(0, 4) : categoriesMap[category];
+    setProducts(listOfProducts)
+  }, [categoriesMap, category, setProducts, onCategoryTitleClick]);
+
 
   return (
     <Fragment>
       <h2 className={onCategoryTitleClick ? 'category-title' : 'category-title-center'} onClick={() => onCategoryTitleClick && onCategoryTitleClick(category)}>{ category.toUpperCase() }</h2>
 
       <div className="products-container">
-        { productsToDisplay().map(product => <ProductCard key={ product.id } product={ product }></ProductCard>) }
+        { products && products.map(product => <ProductCard key={ product.id } product={ product }></ProductCard>) }
       </div>
     </Fragment>
   );
