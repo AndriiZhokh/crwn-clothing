1b67fbc23d67e9f410194c2e172b610beec168f7
Andrii Zhokh <andriizhokh@gmail.com>
2024-01-29 16:22:10 +0200

    refactored user context to use reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
 
 // actual value to access
@@ -7,9 +7,41 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const UESER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+};
+
+const userReducer = (state, action) => {
+  console.log('dispatched');
+  console.log(action);
+  const { type, payload } = action;
+
+  switch (type) {
+    case UESER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      }
+    default:
+      throw new Error(`Unhadled type: ${type} in the user reducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [ currentUser, setCurrentUser ] = useState(null);
+  const [ { currentUser }, dispatch ] = useReducer(userReducer, INITIAL_STATE);
+  console.log(currentUser);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: UESER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+
+  };
+
   const value = { currentUser, setCurrentUser };
+
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
